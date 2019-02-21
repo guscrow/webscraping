e46986474dcec1024002e7b8015fc18a3596b00e
guscrow <47224677+guscrow@users.noreply.github.com>
2019-02-21 15:38:36 -0500

    Update majors.js

--- majors.js
+++ majors.js
@@ -1,1 +1,6 @@
-
+var s = document.createElement("script")
+s.src = "https://d3js.org/d3.v5.js"
+var body = document.getElementsByTagName("body")[0]
+body.after(s)
+var l = d3.selectAll(".fusion-text a").nodes()
+l.forEach(function(d){ var x = d.innerText; console.log(x)});
